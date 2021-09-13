113442e4705b1f540abbd0417bdd2325643fd0fc
Sujoy Datta <sujoydatta.2009@gmail.com>
2021-09-13 16:18:34 +0530

    parallelised code and updated report

--- gd.cpp
+++ gd.cpp
@@ -8,11 +8,7 @@
 using namespace std;
 
 //Variables for obtaining line of best fit
-double b0 = 0; 
-double b1 = 0; 
-double b2 = 0; 
-double b3 = 0; 
-
+double b[4][10530]={};
 //Swapping function
 void swap(double *xp, double *yp) 
 { 
@@ -27,7 +23,6 @@ void bubbleSort(double arr[], int n)
 { 
     int i, j; 
     for (i = 0; i < n-1; i++)     
-      
     //Absolute swapping mechanism
     for (j = 0; j < n-i-1; j++) 
         if (abs(arr[j]) > abs(arr[j+1])) 
@@ -39,46 +34,47 @@ void bubbleSort(double arr[], int n)
 void train(double *x1,double *x2,double *x3,double *y) 
 {   
     double start,end;
-    int i,idx;double error[3861]; // for storing the error values
+    int i,idx;double error[10530]; // for storing the error values
     //Since there are 351 values in our dataset and we want to run for 50 batches so total for loop run 17550 times
-    double err;          // for calculating error on each stage
+    double err[10530]={0};          // for calculating error on each stage
     double alpha = 0.01; // initializing our learning rate
     double e = 2.718281828;
-    double p=0,pred=0.5;
+    double p[10530]={0},pred[10530]={0.5};
+
     start=omp_get_wtime();
-    #pragma omp parallel for  
-    for (i = 0; i < 3861; i++) 
+    #pragma omp parallel for 
+    for (i = 0; i < 10530; i++) 
     {   
-        idx = i % 11;//for accessing index after every batch
-        p = -(b0 + b1 * x1[idx] + b2 * x2[idx] + b3 * x3[idx]);//making the prediction
-        pred = 1 / (1 + pow(e, p)); //calculating final prediction applying sigmoid 
-        err = y[idx] - pred; //calculating the error
-        for(int j=0;j<10000;j++)
+        idx = i % 10;//for accessing index after every batch
+        p[i] = -(b[0][i] + b[1][i] * x1[idx] + b[2][i] * x2[idx] + b[3][i] * x3[idx]);//making the prediction
+        pred[i] = 1 / (1 + pow(e, p[i])); //calculating final prediction applying sigmoid 
+        err[i] = y[idx] - pred[i]; //calculating the error
+        for(int j=0;j<100000;j++)
         {
-            b0 = b0 - alpha * err * pred * (1 - pred) * 1.0;     //updating b0
-            b1 = b1 + alpha * err * pred * (1 - pred) * x1[idx]; //updating b1
-            b2 = b2 + alpha * err * pred * (1 - pred) * x2[idx]; //updating b2
-            b3 = b3 + alpha * err * pred * (1 - pred) * x3[idx]; //updating b3
+            b[0][i]=b[0][i] - alpha * err[i] * pred[i] * (1 - pred[i]) * 1.0;     //updating b0
+            b[1][i]=b[1][i] + alpha * err[i] * pred[i] * (1 - pred[i]) * x1[idx]; //updating b1
+            b[2][i]=b[2][i] + alpha * err[i] * pred[i] * (1 - pred[i]) * x2[idx]; //updating b2
+            b[3][i]=b[3][i] + alpha * err[i] * pred[i] * (1 - pred[i]) * x3[idx]; //updating b3
         }
-        cout << "\tB0= " << b0 << " " << "\t\tB1= " << b1 << " " << "\t\tB2= " << b2 << "\t\tB3= " << b3 << "\t\tError=" << err << endl;
-        error[i]=err;
+        //cout << "\tB0= " << b[0][i] << " " << "\t\tB1= " << b[1][i] << " " << "\t\tB2= " << b[2][i] << "\t\tB3= " << b[3][i] << "\t\tError=" << err << endl;
+        error[i]=err[i];
     } 
-    end=omp_get_wtime();  
+    bubbleSort(error,i);
     //custom sort based on absolute error difference
-    bubbleSort(error,3861); 
-    cout << "Final Values are: " << "\tB0=" << b0 << " " << "\tB1=" << b1 << " " << "\tB2=" << b2 << "\tB3=" << b3 <<"\tError=" << error[0]<<endl;
+    end=omp_get_wtime();
     //Time Taken
-    cout<<"Time= "<<end-start<<endl;
+    cout<<"Time- "<<end-start<<endl;   
+    cout << "Final Values are: " << "\tB0=" << b[0][10529] << " " << "\tB1=" << b[1][10529] << " " << "\tB2=" << b[2][10529] << "\tB3=" << b[3][10529] <<"\tMinimum Error=" << abs(error[0])<<endl;
 }
 
 //Testing the trained Stochastic Model
 void test(double test1, double test2, double test3) 
 {
     //make prediction
-    double pred = b0 + b1 * test1 + b2 * test2 + b3*test3;
+    double pred = b[0][10529] + b[1][10529] * test1 + b[2][10529] * test2 + b[3][10529]*test3;
     char ch;
 
-    cout << "The value predicted by the model= " << pred << endl;
+    //cout << "The value predicted by the model= " << pred << endl;
     if (pred > 0.5)
     {
         pred = 1;
@@ -95,10 +91,10 @@ void test(double test1, double test2, double test3)
 int main() 
 {
     //Input dataset arrays
-    double x1[351];
-    double x2[351];
-    double x3[351];
-    double y[351];
+    double x1[1053];
+    double x2[1053];
+    double x3[1053];
+    double y[1053];
 
     //Reading the data file
     FILE* fp = fopen("ionosphere_data.csv", "r");
@@ -157,7 +153,7 @@ int main()
     train(x1, x2,x3, y);
 
     //Testing Phase
-    double test1=0.5131, test2=-0.00015, test3=0.52099; 
+    double test1=0.35346, test2=0.69387, test3=0.68195; 
     test(test1, test2, test3);
 
 
